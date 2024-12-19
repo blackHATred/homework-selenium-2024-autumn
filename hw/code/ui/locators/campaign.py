6cda5bed85d4169c0e1c835cd428a97d55b76936
KristinaBu <krisdinabui@gmail.com>
2024-12-19 14:08:53 +0300

    fix locators

--- hw/code/ui/locators/campaign.py
+++ hw/code/ui/locators/campaign.py
@@ -8,18 +8,18 @@ class CampaignMenuLocators:
 
     CAMPAIGN_DROPDOWN_BUTTON = (By.XPATH, '//button[@data-testid="tags-selector" and .//span[text()="Все кампании"]]')
     DRAFTS_BUTTON = (By.XPATH, "//div[@role='button' and .//span[@data-testid='drafts']]")
-    DRAFT_ROWS = (By.XPATH, '//div[contains(@class, "BaseTable__row") and .//div[contains(@class, "statusCell_text__St5Pm") and text()="Черновик"]]')
+    DRAFT_ROWS = (By.XPATH, '//div[contains(@class, "BaseTable__row") and .//div[contains(@class, "statusCell_text") and text()="Черновик"]]')
     CAMPAIGN_SEARCH_INPUT = (By.XPATH, '//input[@placeholder="Поиск кампаний" and @data-testid="filter-search-input"]')
     CHECKBOX_ON = (By.XPATH, '//div[contains(@class, "vkuiCheckbox__icon--on") and .//svg[@class="vkuiIcon vkuiIcon--20 vkuiIcon--w-20 vkuiIcon--h-20 vkuiIcon--check_box_on_20"]]')
     # к сожалению, более емкие локаторы не подходят
     MAIN_CHECKBOX = (By.XPATH, '//label[@class="vkuiCheckbox vkuiCheckbox--sizeY-compact vkuiCheckbox--simple vkuiTappable vkuiInternalTappable vkuiTappable--hasHover vkuiTappable--hasActive vkui-focus-visible" and .//input[@id="checkbox-all"]]')
     MAIN_CHECKBOX_LABEL = (By.XPATH, '//label[@for="checkbox-all"]')
     DELETE_BUTTON = (By.XPATH, '//span[contains(@class, "vkuiButton__content") and text()="Удалить"]')
-    CONFIRM_DELETE_BUTTON = (By.XPATH, '//div[contains(@class, "confirmRemoveModal_footer__dW3aB")]//button[contains(@class, "vkuiButton--appearance-negative") and .//span[text()="Удалить"]]')
+    CONFIRM_DELETE_BUTTON = (By.XPATH, '//div[contains(@class, "confirmRemoveModal_footer")]//button[contains(@class, "vkuiButton--appearance-negative") and .//span[text()="Удалить"]]')
     CANCEL_BUTTON = (By.XPATH, '//button[contains(@class, "vkuiButton--appearance-accent") and .//span[text()="Отмена"]]')
 
     DATE_PICKER_BUTTON = (By.XPATH, '//button[@data-testid="filter-data-picker"]')
-    DATE_TEXT = (By.XPATH, './/div[@class="RangeDatePickerManager_rangeText__VQM6k"]')
+    DATE_TEXT = (By.XPATH, './/div[contains(@class, "RangeDatePickerManager_rangeText")]')
     DATE_RANGE_BUTTON = (By.XPATH, '//button[contains(@class, "rdrStaticRange") and .//span[text()="Этот год"]]')
     TODAY_BUTTON = (By.XPATH, '//button[contains(@class, "rdrStaticRange") and .//span[text()="Сегодня"]]')
     START_DATE_INPUT = (By.XPATH, "//input[@name='startDate']")
@@ -27,7 +27,7 @@ class CampaignMenuLocators:
     SHOW_CALENDAR_BUTTON = (By.XPATH, '//button[@type="button" and @aria-label="Показать календарь" and contains(@class, "vkuiIconButton")]')
     CANCEL_CALENDAR_BUTTON = (By.XPATH, '//button[contains(@class, "vkuiButton--appearance-accent") and .//span[text()="Отменить"]]')
     APPLY_CALENDAR_BUTTON = (By.XPATH, '//button[contains(@class, "vkuiButton--appearance-accent") and .//span[text()="Применить"]]')
-    DATE_RANGE_TEXT = (By.XPATH, '//div[contains(@class, "RangeDatePickerManager_rangeText__VQM6k")]')
+    DATE_RANGE_TEXT = (By.XPATH, '//div[contains(@class, "RangeDatePickerManager_rangeText")]')
     
 class CampaignSettingsLocators:
     CAMPAIGN_SIDEBAR_ITEM = (By.XPATH, '//div[@class="MenuItem_cellWrapper__dSQle" and contains(@title, "Кампания")]')
@@ -41,7 +41,7 @@ class CampaignSettingsLocators:
     BUDGET_INPUT = (By.XPATH, '//input[@placeholder="Не задан" and @data-testid="targeting-not-set" and contains(@class, "vkuiInput__el")]')
     BUDGET_INPUT_ERROR = (By.XPATH, '//span[@role="alert" and contains(@class, "vkuiFormItem__bottom") and contains(text(), "Укажите бюджет не меньше 100₽")]')
     SELLING_PROPOSITION_TEXTAREA = (By.XPATH, '//textarea[@placeholder="Опишите ваше предложение" and contains(@class, "vkuiTextarea__el")]')
-    BUDGET_SELECT = (By.XPATH, '//div[contains(@class, "Budget_select__GQI9c")]//input[@data-testid="budget"]')
+    BUDGET_SELECT = (By.XPATH, '//div[contains(@class, "Budget_select")]//input[@data-testid="budget"]')
     START_DATE_INPUT = (By.XPATH, '//span[@data-testid="start-date" and contains(@class, "vkuiDateInput__input")]')
     CALENDAR = (By.XPATH, '//div[contains(@class, "vkuiCalendar")]')
     NEXT_MONTH_BUTTON = (By.XPATH, '//div[@role="button" and @aria-label[contains(., "Следующий месяц")]]')
@@ -49,15 +49,15 @@ class CampaignSettingsLocators:
 
     # Узнаваемость и охват
     RADIO_OPTION_SITE = (By.XPATH, '//label[contains(@class, "vkuiRadio") and .//span[text()="Сайт"]]')
-    BANNER_AD_OPTION = (By.XPATH, '//div[@data-id="branding_multi" and contains(@class, "Option_item__gJMDY")]')
-    ADVERTISED_SITE_LABEL = (By.XPATH, '//h2[contains(@class, "FormItem_top__A6tjT") and contains(.//span, "Рекламируемый сайт")]')
-    SHOWS_PER_USER_LABEL = (By.XPATH, '//h2[contains(@class, "FormItem_top__A6tjT") and contains(.//span, "Показов на пользователя")]')
+    BANNER_AD_OPTION = (By.XPATH, '//div[@data-id="branding_multi" and contains(@class, "Option_item")]')
+    ADVERTISED_SITE_LABEL = (By.XPATH, '//h2[contains(@class, "FormItem_top") and contains(.//span, "Рекламируемый сайт")]')
+    SHOWS_PER_USER_LABEL = (By.XPATH, '//h2[contains(@class, "FormItem_top") and contains(.//span, "Показов на пользователя")]')
 
     # Футер
-    ERROR_BUTTON = (By.XPATH, '//button[contains(@class, "ErrorsTooltip_button__YyIDS") and contains(@class, "vkuiButton")]')
-    CONTINUE_BUTTON = (By.XPATH, '//div[contains(@class, "CreateFooter_actionsWrap__9H7gw")]//button[contains(@class, "vkuiButton") and .//span[text()="Продолжить"]]')
+    ERROR_BUTTON = (By.XPATH, '//button[contains(@class, "ErrorsTooltip_button") and contains(@class, "vkuiButton")]')
+    CONTINUE_BUTTON = (By.XPATH, '//div[contains(@class, "CreateFooter_actionsWrap")]//button[contains(@class, "vkuiButton") and .//span[text()="Продолжить"]]')
     SAVE_AS_DRAFT_BUTTON = (By.XPATH, '//button[contains(@class, "vkuiButton") and .//span[text()="Сохранить как черновик"]]')
-    MAIN_MENU_BUTTON = (By.XPATH, '//button[contains(@class, "header_logoNewSidebar__xxqHV") and @data-route="root"]')
+    MAIN_MENU_BUTTON = (By.XPATH, '//button[contains(@class, "header_logoNewSidebar") and @data-route="root"]')
     
 
 class GroupLocators:
@@ -67,23 +67,22 @@ class GroupLocators:
     SET_TIME_BUTTON = (By.XPATH, '//span[contains(@class, "vkuiSimpleCell__children") and contains(text(), "Настроить время показа")]')
     MY_TIME_BUTTON = (By.XPATH, '//button[contains(@class, "Preset_button__+hw+w") and .//span[text()="Моё время"]]')
     # длинные локаторы, так как нудно учитывать конкретный слот в большом массиве
-    TIME_SLOT_ACTIVE = (By.XPATH, '//div[@data-id="0" and @data-day-id="0" and @data-hour-id="0" and contains(@class, "Cell_cell_checked__jp-QY")]')
-    TIME_SLOT_NOT_ACTIVE = (By.XPATH, '//div[@data-id="0" and @data-day-id="0" and @data-hour-id="0" and contains(@class, "Cell_cell__9eaRF") and not(contains(@class, "Cell_cell_checked__jp-QY"))]')
-    ADD_GROUP_BUTTON = (By.XPATH, '//button[contains(@class, "AddGroupButton_addButton__qM3RA") and .//span[text()="Добавить ещё группу"]]')
+    TIME_SLOT_ACTIVE = (By.XPATH, '//div[@data-id="0" and @data-day-id="0" and @data-hour-id="0" and contains(@class, "Cell_cell") and (contains(@class, "Cell_cell_checked"))]')
+    TIME_SLOT_NOT_ACTIVE = (By.XPATH, '//div[@data-id="0" and @data-day-id="0" and @data-hour-id="0" and contains(@class, "Cell_cell") and not(contains(@class, "Cell_cell_checked"))]')
+    ADD_GROUP_BUTTON = (By.XPATH, '//button[contains(@class, "AddGroupButton_addButton") and .//span[text()="Добавить ещё группу"]]')
     REGION_INPUT = (By.XPATH, '//input[@placeholder="Страна, регион или город" and contains(@class, "vkuiSearch__nativeInput")]')
-    SEARCH_TOOLTIP_NO_RESULTS = (By.XPATH, '//div[contains(@class, "Tooltip_tooltip__piez5") and @role="tooltip"]//span[contains(text(), "Ничего не нашлось")]')
-    SEARCH_TOOLTIP_SHORT_REQUEST = (By.XPATH, '//div[contains(@class, "Tooltip_tooltip__piez5") and @role="tooltip"]//span[text()="Введите поисковый запрос"]')
-    YAROSLAVL_OPTION = (By.XPATH, '//div[contains(@class, "Branch_title__FvS4M") and .//span/b[text()="Ярославль"]]')
-    YAROSLAVL_LABEL = (By.XPATH, '//h4[contains(@class, "RegionsList_label__KPYrN") and text()="Ярославль"]')
-    YAROSLAVL_REMOVE_BUTTON = (By.XPATH, '//h4[contains(@class, "RegionsList_label__KPYrN") and text()="Ярославль"]/following-sibling::button[contains(@class, "RegionsList_close__XtcC-")]')
+    SEARCH_TOOLTIP_NO_RESULTS = (By.XPATH, '//div[contains(@class, "Tooltip_tooltip") and @role="tooltip"]//span[contains(text(), "Ничего не нашлось")]')
+    SEARCH_TOOLTIP_SHORT_REQUEST = (By.XPATH, '//div[contains(@class, "Tooltip_tooltip") and @role="tooltip"]//span[text()="Введите поисковый запрос"]')
+    YAROSLAVL_OPTION = (By.XPATH, '//div[contains(@class, "Branch_title") and .//span/b[text()="Ярославль"]]')
+    YAROSLAVL_LABEL = (By.XPATH, '//h4[contains(@class, "RegionsList_label") and text()="Ярославль"]')
+    YAROSLAVL_REMOVE_BUTTON = (By.XPATH, '//h4[contains(@class, "RegionsList_label") and text()="Ярославль"]/following-sibling::button[contains(@class, "RegionsList_close")]')
     DEMOGRAPHY_SECTION = (By.XPATH, '//div[@role="button" and .//h3[text()="Демография"]]')
     CONTINUE_BUTTON = (By.XPATH, '//button[contains(@class, "vkuiButton") and .//span[text()="Продолжить"]]')
 
 
 class AdLocators:
-    ADD_AD_BUTTON = (By.XPATH, '//button[contains(@class, "AdForm_addButton__lw-oq") and .//span[text()="Добавить ещё объявление"]]')
-    ADD_ITEMS = (By.XPATH, '//div[@class="MenuItem_cellWrapper__dSQle" and contains(@title, "Объявление")]')
-
+    ADD_AD_BUTTON = (By.XPATH, '//button[contains(@class, "AdForm_addButton") and .//span[text()="Добавить ещё объявление"]]')
+    ADD_ITEMS = (By.XPATH, '//div[contains(@class, "MenuItem_cellWrapper") and contains(@title, "Объявление")]')
 
 class CampaignLocators(CampaignMenuLocators, CampaignSettingsLocators, GroupLocators, AdLocators):
     pass
--- hw/code/ui/locators/commerce_center.py
+++ hw/code/ui/locators/commerce_center.py
@@ -2,27 +2,27 @@ from selenium.webdriver.common.by import By
 
 class CommerceCenterPageLocators:
     CREATE_CATALOG_BUTTON = (By.XPATH, '//button[@data-testid="create-catalog" and .//span[text()="Создать каталог"]]')
-    NEW_CATALOG_HEADER = (By.XPATH, '//h2[contains(@class, "ModalSidebarPage_title__Uu-FC") and text()="Новый каталог"]')
+    NEW_CATALOG_HEADER = (By.XPATH, '//h2[contains(@class, "ModalSidebarPage_title") and text()="Новый каталог"]')
     CATALOG_NAME_INPUT = (By.XPATH, '//input[@data-testid="catalogName-input"]')
     REQUIRED_FIELD_NAME_ALERT = (By.XPATH, '//span[contains(@class, "vkuiFormItem__bottom") and @role="alert" and .//div[text()="Обязательное поле"]]')
     MANUAL_OPTION = (By.XPATH, '//div[@data-testid="catalog-source_type-select" and .//h4[text()="Вручную"]]')
     FEED_INPUT = (By.XPATH, '//input[@type="file" and @accept="text/csv,text/tab-separated-values,application/xml,.yml"]')
     SUBMIT_BUTTON = (By.XPATH, '//button[@data-testid="submit" and @title="Создать каталог" and .//span[text()="Создать каталог"]]')
-    FIELD_ERROR_ALERT = (By.XPATH, '//div[contains(@class, "formBanner_container__Y0PJ7 error medium") and .//span[text()="Обязательное поле"]]')
+    FIELD_ERROR_ALERT = (By.XPATH, '//div[contains(@class, "formBanner_container error medium") and .//span[text()="Обязательное поле"]]')
     CANCEL_BUTTON = (By.XPATH, '//button[@data-testid="cancel" and .//span[text()="Отмена"]]')
     TABLE_HEADERS = (By.XPATH, '//div[@role="gridcell" and contains(@class, "BaseTable__header-cell")]')
     CATALOG_ITEMS = (By.XPATH, '//a[@data-testid="catalog-item"]')
     SEARCH_INPUT = (By.XPATH, '//input[@data-testid="search"]')
     NOTHING_FOUND_HEADER = (By.XPATH, '//h2[contains(@class, "vkuiPlaceholder__header") and text()="Ничего не нашлось"]')
-    STATUS_ACTIVE = (By.XPATH, '//div[@class="StatusCell_cell__JNHWT" and contains(text(), "Активный")]')
-    COMMERCE_CENTER_HEADER = (By.XPATH, '//h1[@class="vkuiTypography vkuiTypography--normalize CatalogsPlaceholder_placeHolderTitle__jM+ip vkuiTitle--level-1" and contains(text(), "Центр коммерции — место, где хранятся каталоги товаров и услуг")]')
+    ADD_ITEMS = (By.XPATH, '//div[contains(@class, "MenuItem_cellWrapper") and contains(@title, "Объявление")]')
+    COMMERCE_CENTER_HEADER = (By.XPATH, '//h1[contains(@class, "vkuiTypography") and contains(@class, "vkuiTypography--normalize") and contains(@class, "CatalogsPlaceholder_placeHolderTitle") and contains(@class, "vkuiTitle--level-1") and contains(text(), "Центр коммерции — место, где хранятся каталоги товаров и услуг")]')
     
 class CatalogLocators:
     
     SETTINGS_BUTTON = (By.XPATH, '//button[contains(@class, "vkuiButton--appearance-neutral") and .//span[text()="Настройки"]]')
     DELETE_CATALOG_BUTTON = (By.XPATH, '//button[@data-testid="delete" and @title="Удалить каталог" and .//span[text()="Удалить каталог"]]')
     REMOVE_BUTTON = (By.XPATH, '//button[@data-testid="button-remove" and .//span[text()="Удалить"]]')
-    SETTINGS_HEADER = (By.XPATH, '//h2[contains(@class, "ModalSidebarPage_title__Uu-FC") and text()="Настройки каталога"]')
+    SETTINGS_HEADER = (By.XPATH, '//h2[contains(@class, "ModalSidebarPage_title") and text()="Настройки каталога"]')
 
     #Goods
     ITEMS_TABLE = (By.XPATH, '//div[contains(@class, "BaseTable") and @role="table"]')
@@ -39,16 +39,16 @@ class CatalogLocators:
     CREATE_GROUP_BUTTON = (By.XPATH, '//button[contains(@class, "vkuiButton--appearance-accent") and .//span[text()="Создать группу"]]')
     USE_FILTERS_BUTTON = (By.XPATH, '//div[@role="button" and .//span[text()="Использовать фильтры"]]')
     CHOOSE_GOODS_MANUALLY_BUTTON = (By.XPATH, '//div[@role="button" and .//span[text()="Выбрать товары вручную"]]')
-    NEW_GROUP_HEADER = (By.XPATH, '//h2[contains(@class, "ModalSidebarPage_title__Uu-FC") and text()="Новая группа товаров"]')
-    ADD_FILTER_BUTTON = (By.XPATH, '//button[contains(@class, "Filters_filterConditionAdd__nwksc") and .//span[text()="Добавить фильтр"]]')
-    FILTER_CONDITION = (By.XPATH, '//div[contains(@class, "Condition_wrap__5hsh- Condition_withIndicator__KUEys")]')
+    NEW_GROUP_HEADER = (By.XPATH, '//h2[contains(@class, "ModalSidebarPage_title") and text()="Новая группа товаров"]')
+    ADD_FILTER_BUTTON = (By.XPATH, '//button[contains(@class, "Filters_filterConditionAdd") and .//span[text()="Добавить фильтр"]]')
+    FILTER_CONDITION = (By.XPATH, '//div[contains(@class, "Condition_wrap") and contains(@class, "Condition_withIndicator")]')
     # есть наалогичные элементы, поэтому иначе никак
     GROUP_NAME_INPUT = (By.XPATH, '//div[@class="vkuiFormItem vkuiFormItem--withPadding vkuiInternalFormItem vkuiFormItem--sizeY-none vkuiInternalFormItem--sizeY-none"]//input[@class="vkuiTypography vkuiInput__el vkuiText vkuiText--sizeY-none" and @name="groupName"]')
     SAVE_BUTTON = (By.XPATH, '//button[@type="submit" and @data-testid="submit" and .//span[text()="Сохранить"]]')
     CANCEL_BUTTON = (By.XPATH, '//button[@type="button" and @data-testid="cancel" and .//span[text()="Отмена"]]')
     GROUP_ITEM_BY_NAME = (By.XPATH, '//a[@data-testid="catalog-groups-nav-item" and .//h2[contains(@class, "vkuiHeader__content")]]')
     GROUP_ITEM_BY_NAME_CONTAINS = (By.XPATH, '//a[@data-testid="catalog-groups-nav-item" and .//h2[contains(@class, "vkuiHeader__content") and contains(text(), "{name}")]]')
-    MORE_BUTTON = (By.XPATH, '//button[contains(@class, "Toolbar_actionsButton__32Sv4") and .//svg[@class="vkuiIcon--more_horizontal_24"]]')
+    MORE_BUTTON = (By.XPATH, '//button[contains(@class, "Toolbar_actionsButton") and .//svg[@class="vkuiIcon--more_horizontal_24"]]')
     DELETE_GROUP_BUTTON = (By.XPATH, '//label[@data-testid="dropdown-item" and .//span[text()="Удалить группу"]]')
     CONFIRM_DELETE_BUTTON = (By.XPATH, '//button[@data-testid="button-remove" and .//span[text()="Удалить"]]')
     # есть несколько полей поиска
@@ -61,13 +61,14 @@ class CatalogLocators:
     SELECTED_GOODS_TAB = (By.XPATH, '//div[@role="tab" and .//span[text()="Выбранные"]]')
     GROUP_IN_LIST_GROUP = (By.XPATH, '//a[@data-testid="catalog-groups-nav-item" and .//h2[contains(@class, "vkuiHeader__content")]]')
     # есть несколько полей поиска
-    SEARCH_INPUT_IN_GROUPS = (By.XPATH,"//div[@class='CatalogGroups_controls__MC4-s']//input[@data-testid='search']")
+    SEARCH_INPUT_IN_GROUPS = (By.XPATH, "//div[contains(@class, 'CatalogGroups_controls')]//input[@data-testid='search']")
 
     # Download
     DOWNLOAD_HISTORY_TAB = (By.XPATH, '//div[@role="tab" and @data-testid="catalog-tabs-history" and .//span[text()="История загрузок"]]')
-    REFRESH_FILE_BUTTON = (By.XPATH, '//button[contains(@class, "UpdateBannerInfo_refreshButton__uZEpU") and .//span[text()="Обновить файл"]]')
+    REFRESH_FILE_BUTTON = (By.XPATH, '//button[contains(@class, "UpdateBannerInfo_refreshButton") and .//span[text()="Обновить файл"]]')
     CATALOG_DOWNLOAD_HEADER = (By.XPATH, '//div[@class="vkuiPanelHeader__content"]//h2[@class="vkuiPanelHeader__content-in" and text()="Настройки каталога"]')
 
 
 class CommerceCenterLocators(CatalogLocators, CommerceCenterPageLocators):
     pass
+    
